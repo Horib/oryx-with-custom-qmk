eb5b7d15fd77bbccad8eb5df3d65f1cef94f6e0d
Rasmus Hansen <r@smus.dev>
2025-09-01 21:16:35 +0100

    test

--- BqdB0/keymap.c
+++ BqdB0/keymap.c
@@ -45,7 +45,7 @@ const uint32_t unicode_map[] PROGMEM = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           LT(6, IS_20),   
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           IS_22,          
+    KC_TAB,         UC_OSLS,        UC_ETH,         UC_AE,          UC_ARNG,        KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           IS_22,          
     KC_LEFT_SHIFT,  MT(MOD_LGUI, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LCTL, KC_D),MT(MOD_LSFT, KC_F),KC_G,                                           KC_H,           MT(MOD_RSFT, KC_J),MT(MOD_RCTL, KC_K),MT(MOD_RALT, KC_L),MT(MOD_RGUI, IS_21),MT(MOD_RSFT, IS_16),
     KC_LEFT_CTRL,   MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_F16,         KC_RIGHT_CTRL,  
                                                     LT(4, KC_SPACE),TD(DANCE_0),                                    LT(4, KC_DELETE),LT(3, KC_ENTER)
