5be409134c870cf9fa0fb9cf8960c44c8a212b47
Rasmus Hansen <r@smus.dev>
2025-09-01 21:59:42 +0100

    test

--- BqdB0/keymap.c
+++ BqdB0/keymap.c
@@ -35,16 +35,10 @@ const uint32_t unicode_map[] PROGMEM = {
     [U_A_RING_UPPER]   = 0x00C5,  // Å
 };
 
-// Define custom keycodes for these symbols (shift-able)
-#define UC_ETH    UP(U_ETH_LOWER, U_ETH_UPPER)       // ð / Ð
-#define UC_OSLS   UP(U_O_SLASH_LOWER, U_O_SLASH_UPPER) // ø / Ø
-#define UC_AE     UP(U_AE_LOWER, U_AE_UPPER)         // æ / Æ
-#define UC_ARNG   UP(U_A_RING_LOWER, U_A_RING_UPPER)  // å / Å
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           LT(6, IS_20),   
-    KC_TAB,         UC_OSLS,        UC_ETH,         UC_AE,          UC_ARNG,        KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           IS_22,          
+    KC_TAB,         RALT(KC_O),     RALT(KC_D),     RALT(KC_QUOT),  RALT(KC_A),     KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           IS_22,          
     KC_LEFT_SHIFT,  MT(MOD_LGUI, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LCTL, KC_D),MT(MOD_LSFT, KC_F),KC_G,                                           KC_H,           MT(MOD_RSFT, KC_J),MT(MOD_RCTL, KC_K),MT(MOD_RALT, KC_L),MT(MOD_RGUI, IS_21),MT(MOD_RSFT, IS_16),
     KC_LEFT_CTRL,   MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_F16,         KC_RIGHT_CTRL,  
                                                     LT(4, KC_SPACE),MT(MOD_LGUI, KC_BSPC),                                MT(MOD_RGUI, KC_DELETE),LT(3, KC_ENTER)
